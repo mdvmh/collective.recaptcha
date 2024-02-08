46808ec0c440e231bf2c793788bce1e133762519
mdvmh <52754422+mdvmh@users.noreply.github.com>
2024-02-08 16:36:19 +0100

    Adjustments for Plone 6

--- src/collective/recaptcha/settings.py
+++ src/collective/recaptcha/settings.py
@@ -1,43 +1,95 @@
 # coding=utf-8
+from zope.site.hooks import getSite
 from collective.recaptcha import RecaptchaMessageFactory as _
-from plone.app.registry.browser.controlpanel import RegistryEditForm
+from persistent import Persistent
 from plone.registry.interfaces import IRegistry
 from zope import schema
+from zope.annotation import factory
+from zope.annotation import IAttributeAnnotatable
+from zope.component import adapts
 from zope.component import getUtility
+from zope.interface import implementer
 from zope.interface import Interface
 
 
 try:
-    from plone.formwidget.recaptcha.interfaces import IReCaptchaSettings
-
-    TRY_FORMWIDGET = True
+    from zope.formlib.form import FormFields
 except ImportError:
-    TRY_FORMWIDGET = False
+    # formlib missing (Plone 5?)
+    FormFields = None
+
+
+try:
+    # formlib missing (Plone 5?)
+    from plone.app.registry.browser.controlpanel import RegistryEditForm as EditForm  # noqa
+except ImportError:
+    try:
+        # Zope 2.12+
+        from five.formlib.formbase import EditForm
+    except ImportError:
+        try:
+            # older Zope 2s
+            from Products.Five.formlib.formbase import EditForm
+        except ImportError:
+            # Zope 3
+            from zope.formlib.form import EditForm
+
+try:
+    from plone.formwidget.recaptcha.interfaces import IReCaptchaSettings
+    TRY_REGISTRY = True
+except ImportError:
+    TRY_REGISTRY = False
 
 
 class IRecaptchaSettings(Interface):
 
-    public_key = schema.TextLine(title=_(u"Site Key"))
+    public_key = schema.TextLine(
+        title=_(u'Site Key')
+    )
 
-    private_key = schema.TextLine(title=_(u"Secret Key"))
+    private_key = schema.TextLine(
+        title=_(u'Secret Key')
+    )
+
+
+@implementer(IRecaptchaSettings)
+class RecaptchaSettingsAnnotations(Persistent):
+    adapts(IAttributeAnnotatable)
+
+    def __init__(self):
+        self.public_key = None
+        self.private_key = None
+
+RecaptchaSettings = factory(RecaptchaSettingsAnnotations)
 
 
 def getRecaptchaSettings():
     registry = getUtility(IRegistry)
-    if TRY_FORMWIDGET:
+    if TRY_REGISTRY:
         # if plone.formwidget.recaptcha is installed, try getting
         # its settings from the registry
         try:
             settings = registry.forInterface(IReCaptchaSettings)
             if settings.public_key and settings.private_key:
                 return settings
-        except (AttributeError, KeyError):
+        except:
             pass
-    settings = registry.forInterface(IRecaptchaSettings)
-    if settings.public_key and settings.private_key:
-        return settings
+    # try getting settings from the registry first
+    try:
+        settings = registry.forInterface(IRecaptchaSettings)
+        if settings.public_key and settings.private_key:
+            return settings
+    except KeyError:
+        # fall back to our storage of an annotation on the site if the settings
+        # haven't been configured
+        site = getSite()
+        return IRecaptchaSettings(site)
 
 
-class RecaptchaSettingsForm(RegistryEditForm):
+class RecaptchaSettingsForm(EditForm):
     schema = IRecaptchaSettings
     label = _(u"Recaptcha settings")
+
+    if FormFields:
+        # formlib missing (Plone 5?)
+        form_fields = FormFields(IRecaptchaSettings)
