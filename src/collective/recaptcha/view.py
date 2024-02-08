e93ba776787d32a4305341977ec09c929a648ea6
mdvmh <52754422+mdvmh@users.noreply.github.com>
2024-02-08 16:35:29 +0100

    Adjustments for Plone 6

--- src/collective/recaptcha/view.py
+++ src/collective/recaptcha/view.py
@@ -1,16 +1,11 @@
-# coding=utf-8
-from collective.recaptcha import RecaptchaMessageFactory as _
-from collective.recaptcha.settings import getRecaptchaSettings
-from norecaptcha.captcha import displayhtml
-from norecaptcha.captcha import submit
-from Products.Five import BrowserView
-from zope import schema
+from zope.interface import Interface, implementer
+from zope.component import adapts, queryMultiAdapter
 from zope.annotation import factory
-from zope.component import adapter
-from zope.component import queryMultiAdapter
-from zope.interface import implementer
-from zope.interface import Interface
+from zope import schema
 from zope.publisher.interfaces.browser import IBrowserRequest
+from Products.Five import BrowserView
+from norecaptcha.captcha import displayhtml, submit
+from collective.recaptcha.settings import getRecaptchaSettings
 
 
 class IRecaptchaInfo(Interface):
@@ -19,17 +14,18 @@ class IRecaptchaInfo(Interface):
 
 
 @implementer(IRecaptchaInfo)
-@adapter(IBrowserRequest)
 class RecaptchaInfoAnnotation(object):
+    adapts(IBrowserRequest)
+
     def __init__(self):
         self.error = None
         self.verified = False
-
 
 RecaptchaInfo = factory(RecaptchaInfoAnnotation)
 
 
 class RecaptchaView(BrowserView):
+
     def __init__(self, context, request):
         self.context = context
         self.request = request
@@ -37,34 +33,43 @@ class RecaptchaView(BrowserView):
 
     def image_tag(self):
         portal_state = queryMultiAdapter(
-            (self.context, self.request), name=u"plone_portal_state"
-        )
-        lang = portal_state.language()[:2]
+            (self.context, self.request), name=u'plone_portal_state')
+        if portal_state is not None:
+            lang = portal_state.language()[:2]
+        else:
+            lang = 'en'
 
         if not self.settings.public_key:
             raise ValueError(
-                _(u"No recaptcha public key configured. ")
-                + _(u"Go to /@@recaptcha-settings to configure.")
+                'No recaptcha public key configured. '
+                'Go to /@@recaptcha-settings to configure.'
             )
         return displayhtml(self.settings.public_key, language=lang)
 
-    def verify(self):
+    def audio_url(self):
+        return None
+
+    def verify(self, input=None):
         info = IRecaptchaInfo(self.request)
         if info.verified:
             return True
 
         if not self.settings.private_key:
             raise ValueError(
-                _("No recaptcha private key configured. ")
-                + _("Go to /@@recaptcha-settings to configure.")
+                'No recaptcha private key configured. '
+                'Go to /@@recaptcha-settings to configure.'
             )
-        response_field = self.request.get("g-recaptcha-response")
-        remote_addr = self.request.get("HTTP_X_FORWARDED_FOR", "").split(",")[0]
+        response_field = self.request.get('g-recaptcha-response')
+        remote_addr = self.request.get('HTTP_X_FORWARDED_FOR', '').split(',')[0]
         if not remote_addr:
-            remote_addr = self.request.get("REMOTE_ADDR")
+            remote_addr = self.request.get('REMOTE_ADDR')
         res = submit(response_field, self.settings.private_key, remote_addr)
         if res.error_code:
             info.error = res.error_code
 
         info.verified = res.is_valid
         return res.is_valid
+
+    @property
+    def external(self):
+        return True
